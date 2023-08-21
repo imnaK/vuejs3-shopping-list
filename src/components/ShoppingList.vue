abd7a231894bd56147d2985276a71ab7ce3caeab
imnaK <mc.yama1000@gmail.com>
2023-08-21 20:09:49 +0200

    initial commit

---
+++ src/components/ShoppingList.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import ShoppingListCard from "./ShoppingListCard.vue";
+
+let id = 0;
+
+const shoppingLists = ref([
+  { id: id++, name: "Liste1" },
+  { id: id++, name: "Coole Liste" },
+  { id: id++, name: "Wieso ist hier der name so lang?" },
+]);
+</script>
+
+<template>
+  <div>
+    <h1>Einkaufslisten</h1>
+    <div class="shopping-lists">
+      <ShoppingListCard v-for="shoppingList in shoppingLists" :key="shoppingList.id" :name="shoppingList.name" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.shopping-lists {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(16rem, 1fr));
+  grid-gap: 1rem;
+}
+</style>
---
+++ src/components/StoreItem.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+console.log("hello world!");
+
+</script>
+
+<template>
+  <div>
+    <h1>Item</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
