2e4d71e9bf20d19d99cba540d2d6b6a20825fb15
imnaK <mc.yama1000@gmail.com>
2023-08-22 19:32:34 +0200

    added "tables" and provide/inject

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,51 @@
 <script setup lang="ts">
-import ShoppingList from "./components/ShoppingList.vue";
+import { ref, provide } from "vue";
+import ShoppingListCard from "./components/ShoppingListCard.vue";
+
+const selectedListId = ref(-1);
+
+let tblListsIdCounter = 0;
+const tblLists = ref([
+  { id: tblListsIdCounter++, name: "Liste1" },
+  { id: tblListsIdCounter++, name: "Coole Liste" },
+  { id: tblListsIdCounter++, name: "Wieso ist hier der name so lang? Doch noch länger? Ich kann hier aber echt viel Text rein schreiben lol!" },
+]);
+
+let tblStoresIdCounter = 0;
+const tblStores = ref([
+  { id: tblStoresIdCounter++, listId: 0, name: "Edeka" },
+  { id: tblStoresIdCounter++, listId: 0, name: "Rewe" },
+  { id: tblStoresIdCounter++, listId: 0, name: "Penny" },
+  { id: tblStoresIdCounter++, listId: 1, name: "Aldi" },
+  { id: tblStoresIdCounter++, listId: 1, name: "Lidl" },
+  { id: tblStoresIdCounter++, listId: 2, name: "Netto" },
+]);
+
+let tblItemsIdCounter = 0;
+const tblItems = ref([
+  { id: tblItemsIdCounter++, storeId: 0, name: "Tomaten", completed: false },
+  { id: tblItemsIdCounter++, storeId: 0, name: "Gurken", completed: false },
+  { id: tblItemsIdCounter++, storeId: 0, name: "Sauce Hollandaise", completed: false },
+  { id: tblItemsIdCounter++, storeId: 0, name: "Schinken", completed: false },
+  { id: tblItemsIdCounter++, storeId: 1, name: "Brot", completed: false },
+  { id: tblItemsIdCounter++, storeId: 1, name: "Baguette de Baguette", completed: false },
+]);
+
+provide("selectedListId", selectedListId);
+provide("lists", tblLists);
+provide("stores", tblStores);
+provide("items", tblItems);
+
 </script>
 
 <template>
-  <header>
-  </header>
+  <header></header>
 
   <main>
-    <ShoppingList />
+    <h1>Einkaufslisten</h1>
+    <div class="list-cards">
+      <ShoppingListCard v-for="list in tblLists" :key="list.id" :name="list.name" @click="selectedListId = list.id" />
+    </div>
   </main>
 </template>
 
@@ -19,6 +57,12 @@ header {
 .logo {
   display: block;
   margin: 0 auto 2rem;
+}
+
+.list-cards {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(16rem, 1fr));
+  grid-gap: 1rem;
 }
 
 @media (min-width: 1024px) {
